1b0d34dcdbb1bfd3fa58887f7644a2a3d1f64247
João Paulo Dagostin <dagostinjota@gmail.com>
2025-03-19 09:46:51 +0100

    updates
    main js for scrowlling smoth

---
+++ main.js
@@ -0,0 +1,17 @@
+// Smooth scrowlling:
+
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const targetId = this.getAttribute("href");
+    const targetElement = document.querySelector(targetId);
+
+    if (targetElement) {
+      window.scrollTo({
+        top: targetElement.offsetTop,
+        behavior: "smooth",
+      });
+    }
+  });
+});
